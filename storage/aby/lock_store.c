179938bd579cbb3589b49cf57b0fdd3b5543f5f6
raghu272 <dasari.10@buckeyemail.osu.edu>
2014-11-20 22:06:08 -0500

    raghu 2nd

--- storage/aby/lock_store.c
+++ storage/aby/lock_store.c
@@ -21,11 +21,17 @@ int insert_to_hash_table(pid_t tid, void *addr)
   int idx = ((long int)addr) % 100;
 
   if (table[idx]->next == NULL)
-    
-
+  {
+      table[idx]->tid=tid;  
+      table[idx]->add=addr;
+  }
   // find the end of the linked list at table[idx] and add the new address there
   for (node_t *idx= table[idx]; idx->next!= NULL; idx= idx->next);
-
+  node_t *temp=malloc(sizeof *temp);
+  temp->tid=tid;
+  temp->add=addr;
+  temp->next=NULL;
+  idx->next=temp;
 }
 
 int store_address_in(void* ptr, void* heap_mem, pid_t tid)
@@ -33,4 +39,5 @@ int store_address_in(void* ptr, void* heap_mem, pid_t tid)
   // check whether the address is already present in the hash table
   // if yes, make the thread sleep for soemtime
   // how to resume the thread once the address is unlocked
+ 
 }
